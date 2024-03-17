580d27940bb54c154f2e8e3f9a5a373332dbb7e7
ShivaKumar2718 <malapatisivakumarreddy@gmail.com>
2024-03-17 10:51:19 +0530

    Added the Jetpack Compose dependencies

---
+++ app/src/main/java/com/siva/evoke/activities/EventAndAction.kt
@@ -0,0 +1,35 @@
+package com.siva.evoke.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.siva.evoke.activities.ui.theme.EvokeTheme
+
+class EventAndAction : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    EvokeTheme {
+        Greeting("Android")
+    }
+}
